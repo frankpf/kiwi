81ff8a5bf7c12a225d2724e81b261ac361a2630d
Frank Paulo Filho <me@frankpf.com>
2018-03-13 09:57:54 -0300

    Add error reporting for tokens

--- src/error.ts
+++ src/error.ts
@@ -1,7 +1,18 @@
+import { Token, TokenType } from './token'
 export let HAS_ERROR = false
 
-export function error(line: number, message: string) {
-	report(line, '', message)
+export function error(item: Token         , message: string): void
+export function error(item: number        , message: string): void
+export function error(item: number | Token, message: string): void {
+	if (typeof item === 'number') {
+		report(item, '', message)
+	} else {
+		if (item.type === TokenType.Eof) {
+			report(item.line, ' at end', message)
+		} else {
+			report(item.line, ` at "${item.lexeme}"`, message)
+		}
+	}
 }
 
 function report(line: number, where: string, message: string) {
