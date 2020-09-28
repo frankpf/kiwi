f98ac144a2eb63a9f21c2c5d08e73aead1011244
Frank Filho <me@frankpf.com>
2020-09-28 12:58:45 -0300

    Add matchPartial to match module

--- src/match.ts
+++ src/match.ts
@@ -13,6 +13,8 @@ type MatchingFunc<Union extends TaggedUnion<Tag>, Key extends Union[Tag], R> = (
 	props: Omit<DiscriminateUnion<Union, Tag, Key>, Tag>,
 ) => R
 
+// FIXME: Tag type not used here, we should make the match tag configurable
+// TaggedUnion will be a local type.
 type TaggedUnion<Tag> = {
 	[Tag]: string
 }
@@ -21,11 +23,24 @@ type StrictMatchers<U extends TaggedUnion<Tag>, R> = {
 	[Variant in U[Tag]]: MatchingFunc<U, Variant, R>
 }
 
+type PartialMatchers<U extends TaggedUnion<Tag>, R> =
+	StrictMatchers<U, R> | (Partial<StrictMatchers<U, R>> & { default(item: U): R })
+
 export function matchAll<U extends TaggedUnion<Tag>, R>(matchers: StrictMatchers<U, R>) {
 	const matchersAny = matchers as any
 	return (union: U): R => {
 		const tag = union[Tag]
 		const fn = matchersAny[tag]
 		return fn(union)
+	}
+}
+
+export function matchPartial<U extends TaggedUnion<Tag>, R>(matchers: PartialMatchers<U, R>) {
+	const matchersAny = matchers as any
+	return (union: U): R => {
+		const fn = matchersAny[Tag]
+		return fn !== undefined
+			? fn(union)
+			: matchersAny['default'](union)
 	}
 }
