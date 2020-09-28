5d7125a448866a946598d1e154af4144698a8e02
Frank Filho <me@frankpf.com>
2020-09-28 12:51:24 -0300

    utils: add msg to `unimplemented (_)` function and TupleLength<T> type

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,8 @@
 export type Lazy<A> = () => A
 export const lazy = <A>(arg: A) => () => arg
 
-export function _(...args: any): any {
-	throw new Error('not implemented')
+export function _(msg?: string, ...args: any): any {
+	throw new Error(msg || 'Not implemented')
 }
+
+export type TupleLength<T extends any[]> = T extends { length: infer L } ? L : never
