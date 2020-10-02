8d5a176a91f847b19617e9bee36a88e71c12de04
Frank Filho <me@frankpf.com>
2020-10-02 15:21:20 -0300

    Clean up unused imports from kiwi.nim

--- vm/src/kiwi.nim
+++ vm/src/kiwi.nim
@@ -1,23 +1,15 @@
-from scanner import scanText
 from interpreter import parseTextualBytecode, newInterpreter, interpret, cleanup, RuntimeError
 from utils import kiwiPrintErr
-from os import commandLineParams
-from sequtils import anyIt
-from parseopt import initOptParser
 
 proc main() =
-    # let source = stdin.readAll
-    # let tokens = scanText(source)
     let bytecode = stdin.readAll
-    let parsed = parseTextualBytecode(bytecode)
-    # echo parsed.repr
-    var interp = newInterpreter(parsed)
+    let topLevel = parseTextualBytecode(bytecode)
+    var interp = newInterpreter(topLevel)
     try:
-      interp.interpret
+      interp.interpret()
       interp.cleanup
     except RuntimeError as e:
       kiwiPrintErr e.msg
-
 
 when isMainModule:
     main()
