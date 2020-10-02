8b17f3ae0e46bc39aac9ba672a51008b98710916
Frank Filho <me@frankpf.com>
2020-10-02 15:31:03 -0300

    Overhaul bytecode generator to support functions

--- src/backends/bytecode/instruction.ts
+++ src/backends/bytecode/instruction.ts
@@ -4,8 +4,45 @@ import {matchAll} from '../../match'
 import {TokenType} from '../../token'
 import {Resolver, Local} from '../../localresolver'
 
-const resolver = new Resolver()
+type StrMap<A> = { [_: string]: A }
 
+class DeclarationMap {
+
+	private readonly instructionMap: StrMap<Instruction.T[]> = {}
+	private readonly arityMap: StrMap<number> = {}
+
+	constructor() {}
+
+	addFunction(name: string, arity: number, instructions: Instruction.T[]) {
+		if (this.instructionMap[name] !== undefined || this.arityMap[name] !== undefined) {
+			// FIXME: I don't think this should be an error
+			throw new Error(`Trying to overwrite existing function ${name}`)
+		}
+		this.instructionMap[name] = instructions
+		this.arityMap[name] = arity
+	}
+
+	getInstructions(name: string) {
+		if (this.instructionMap[name] === undefined) {
+			throw new Error(`Trying to access instructions of undefined function ${name}`)
+		}
+
+		return this.instructionMap[name]
+
+	}
+	getArity(name: string): number {
+		if (this.arityMap[name] === undefined) {
+			throw new Error(`Trying to access arity of undefined function ${name}`)
+		}
+
+		return this.arityMap[name]
+	}
+	*entries(): Generator<[name: string, arity: number, instructions: Instruction.T[]]> {
+		for (const [name, instructions] of Object.entries(this.instructionMap)) {
+			yield [name, this.getArity(name), instructions]
+		}
+	}
+}
 
 export namespace Instruction {
 	export type T = Instr<any>
@@ -25,7 +62,7 @@ export namespace Instruction {
 		private _index: number | undefined
 		static globalConstants = {} as any
 
-		constructor(readonly constant: string) {}
+		constructor(readonly constant: string, readonly mode: 'string' | 'function' = 'string') {}
 
 		encode(buf: InstructionBuffer) {
 			const constant = MakeConstant.globalConstants[this.constant]
@@ -34,7 +71,8 @@ export namespace Instruction {
 				return
 			}
 
-			const prefix = `s${this.constant.length}`
+			const marker = this.mode === 'string' ? 's' : 'f'
+			const prefix = `${marker}${this.constant.length}`
 			const index = buf.constants.push(`${prefix} ${this.constant}`) - 1
 			this._index = index
 			MakeConstant.globalConstants[this.constant] = this
@@ -77,6 +115,20 @@ export namespace Instruction {
 
 		sizeInBytes() { return 2 }
 	}
+
+	export class LoadFunction implements Instr<'LoadFunction'> {
+		readonly _tag = 'LoadFunction'
+		constructor(readonly line: number, readonly constant: MakeConstant) {}
+
+		encode(buf: InstructionBuffer) {
+			buf.instructions.push(`load_function ${this.constant.index()}`)
+			buf.lineNumbers.push(this.line)
+		}
+
+		sizeInBytes() { return 2 }
+	}
+
+	export const Call = SimpleInstr<'call', [number]>('call', 1)
 
 	export const SetLocal = SimpleInstr<'set_local', [number]>('set_local', 1)
 	export type SetLocal = InstanceType<typeof SetLocal>
@@ -143,6 +195,8 @@ export namespace Instruction {
 	export const Print = SimpleInstr('print')
 	export type Print = InstanceType<typeof Print>
 
+	export const Debugger = SimpleInstr('Debugger')
+
 	export const Pop = SimpleInstr('pop')
 	export type Pop = InstanceType<typeof Pop>
 
@@ -163,6 +217,9 @@ export namespace Instruction {
 
 	export const Not = SimpleInstr('not')
 	export type Not = InstanceType<typeof Not>
+
+	export const Or = SimpleInstr('Or')
+	export const And = SimpleInstr('And')
 
 	export const Subtract = SimpleInstr('sub')
 	export type Subtract = InstanceType<typeof Subtract>
@@ -216,14 +273,22 @@ export namespace Instruction {
 
 
 type InstructionBuffer = {
+	readonly functionName: string
+	readonly arity: number
 	instructions: string[]
 	constants: string[] // f{number} or i{number}
 	lineNumbers: number[]
 }
 
 export function fromAst(ast: Ast.Stmt[], { source }: { source?: string } = {}): string {
-	const instructions = instructionsFromAst(ast)
-	const buf = genBytecode(instructions)
+	const {instructions, declarationMap} = instructionsFromAst(ast)
+	const lastLine = instructions[instructions.length - 1].line
+	declarationMap.addFunction(
+		'toplevel',
+		0,
+		[...instructions, new Instruction.LoadNil(lastLine), new Instruction.Return(lastLine)]
+	)
+	const buf = genBytecode2(declarationMap)
 	if (source !== undefined) {
 		return toAnnotatedBuf(buf, source)
 	} else {
@@ -231,7 +296,13 @@ export function fromAst(ast: Ast.Stmt[], { source }: { source?: string } = {}): 
 	}
 }
 
-function instructionsFromAst(ast: Ast.Stmt[]): Instruction.T[] {
+type FunctionDef = { arity: number, name: string }
+function instructionsFromAst(ast: Ast.Stmt[], _declMap?: DeclarationMap, _resolver?: Resolver): { instructions: Instruction.T[], declarationMap: DeclarationMap } {
+	const declMap = _declMap === undefined ? new DeclarationMap() : _declMap
+	const resolver = _resolver === undefined ? new Resolver() : _resolver
+	// TODO(repl): We need this for --replCompiler to work
+	// Instruction.MakeConstant.globalConstants = {}
+
 	function exprMatcher(expr: Ast.Expr): Instruction.T[] {
 		const matcher = matchAll<Ast.Expr, Instruction.T[]>({
 			Literal({value, startToken}) {
@@ -286,6 +357,12 @@ function instructionsFromAst(ast: Ast.Stmt[]): Instruction.T[] {
 						break
 					case TokenType.LessEqual:
 						opInstr = new Instruction.LessEqual(line)
+						break
+					case TokenType.Or:
+						opInstr = new Instruction.Or(line)
+						break
+					case TokenType.And:
+						opInstr = new Instruction.And(line)
 						break
 					default:
 						throw new Error(`Binary operator ${operator} not supported`)
@@ -348,6 +425,33 @@ function instructionsFromAst(ast: Ast.Stmt[]): Instruction.T[] {
 				const popInstrs = resolver.endScope()
 				return [...instrs, ...popInstrs]
 			},
+			Function({name, params, body, returnStmt, startToken}) {
+				const innerResolver = new Resolver()
+				innerResolver.beginScope()
+				innerResolver.declareVariable(name)
+				innerResolver.markInitialized()
+				for (const param of params) {
+					innerResolver.declareVariable(param)
+					innerResolver.markInitialized()
+				}
+				const {instructions: bodyInstrs} = instructionsFromAst(body, declMap, innerResolver)
+				const {instructions: returnInstrs} = instructionsFromAst([returnStmt], declMap, innerResolver)
+				const popInstrs = []
+
+				const funcInstrs = [...bodyInstrs, ...popInstrs, ...returnInstrs]
+				declMap.addFunction(name.lexeme, params.length, funcInstrs)
+
+
+
+				const mkIdentifier = new Instruction.MakeConstant(name.lexeme, 'function')
+				return [mkIdentifier, new Instruction.LoadFunction(startToken.line, mkIdentifier)]
+			},
+			Call({ callee, args, startToken }) {
+				const calleeInstrs = exprMatcher(callee)
+				const argsInstrs = args.flatMap(exprMatcher)
+				const callInstr = new Instruction.Call(startToken.line, args.length)
+				return [...calleeInstrs, ...argsInstrs, callInstr]
+			}
 		})
 		return matcher(expr)
 	}
@@ -366,12 +470,27 @@ function instructionsFromAst(ast: Ast.Stmt[]): Instruction.T[] {
 			}
 		},
 		While({condition, block}) {
-			throw 'While not supported'
-			return _()
+			const conditionInstrs = exprMatcher(condition)
+			const blockInstrs = exprMatcher(block)
+
+			let bytesToJumpOver = 3 + blockInstrs.map(_ => _.sizeInBytes()).reduce((a, b) => a + b)
+			// FIXME: this doesn't make sense. I think we can emit -1 for the jump linenum
+			// if we assume it won't ever error, but right now it can have an error if the jump
+			// offset doesn't fit in 16 bits. Once we have a jump opcode that works with arbitrary
+			// offsets, we can use -1.
+			const jumpIfFalse = new Instruction.Jump(-1, Instruction.JumpMode.IfFalse, bytesToJumpOver)
+
+			let conditionLen = conditionInstrs.map(_ => _.sizeInBytes()).reduce((a, b) => a + b)
+			const jumpBack = new Instruction.Jump(-1, Instruction.JumpMode.Always, (-1 * bytesToJumpOver) - 1 - conditionLen)
+
+			return [...conditionInstrs, jumpIfFalse, ...blockInstrs, jumpBack]
 		},
 		Print({expression, printToken}) {
 			// TODO Thread print token here
 			return [...exprMatcher(expression), new Instruction.Print(printToken.line)]
+		},
+		Debugger({debuggerToken}) {
+			return [new Instruction.Debugger(debuggerToken.line)]
 		},
 		LetDeclaration({identifier, initializer}) {
 			resolver.declareVariable(identifier)
@@ -391,44 +510,73 @@ function instructionsFromAst(ast: Ast.Stmt[]): Instruction.T[] {
 			} else {
 				return [mkIdentifier, ...initializerInstrs, new Instruction.DefineGlobal(identifier.line, mkIdentifier)]
 			}
-
 		},
+		Return({expression}) {
+			const exprInstrs = exprMatcher(expression)
+			const line = exprInstrs[exprInstrs.length - 1].line
+			const returnInstr = new Instruction.Return(line)
+			return [...exprInstrs, returnInstr]
+		}
 	})
-	const instrs = ast.flatMap(stmtMatcher)
-	const lastLine = instrs[instrs.length - 1].line
-	return [...instrs, new Instruction.Return(lastLine)]
+
+	const instructions = ast.flatMap(stmtMatcher)
+	return { instructions, declarationMap: declMap }
 }
 
-export function genBytecode(instructions: Instruction.T[]): InstructionBuffer {
-	const instructionBuffer = {
-		instructions: [],
-		constants: [],
-		lineNumbers: [],
-	} as InstructionBuffer
 
-	for (const instruction of instructions) {
-		instruction.encode(instructionBuffer)
+export function genBytecode2(declarationMap: DeclarationMap): InstructionBuffer[] {
+	const bufs = [] as InstructionBuffer[]
+	for (const [functionName, arity, instructions] of declarationMap.entries()) {
+		const instructionBuffer = {
+			functionName,
+			arity,
+			instructions: [],
+			constants: [],
+			lineNumbers: [],
+		}
+		for (const instruction of instructions) {
+			instruction.encode(instructionBuffer)
+		}
+		bufs.push(instructionBuffer)
+
 	}
-
-	return instructionBuffer
+	return bufs
 }
 
-export function toBuf(instrBuf: InstructionBuffer): string {
+//export function genBytecode(instructions: Instruction.T[]): InstructionBuffer {
+//	const instructionBuffer = {
+//		instructions: [],
+//		constants: [],
+//		lineNumbers: [],
+//	} as InstructionBuffer
+//
+//	for (const instruction of instructions) {
+//		instruction.encode(instructionBuffer)
+//	}
+//
+//	return instructionBuffer
+//}
+
+export function toBuf(instrBufs: InstructionBuffer[]): string {
 	let final_bytecode = ''
 	final_bytecode += 'VERSION 0\n'
 	final_bytecode += '\n'
 
-	final_bytecode += 'START INSTRUCTIONS\n'
-	final_bytecode += getBody(instrBuf.instructions)
-	final_bytecode += 'END\n\n'
+	for (const buf of instrBufs) {
+		final_bytecode += '---\n'
+		final_bytecode += `START FUNCTION ${buf.functionName} ${buf.arity}\n`
+		final_bytecode += getBody(buf.instructions)
+		final_bytecode += 'END\n\n'
 
-	final_bytecode += 'START CONSTANTS\n'
-	final_bytecode += getBody(instrBuf.constants)
-	final_bytecode += 'END\n\n'
+		final_bytecode += 'START CONSTANTS\n'
+		final_bytecode += getBody(buf.constants)
+		final_bytecode += 'END\n\n'
 
-	final_bytecode += 'START LINENUM\n'
-	final_bytecode += getBody(instrBuf.lineNumbers)
-	final_bytecode += 'END\n'
+		final_bytecode += 'START LINENUM\n'
+		final_bytecode += getBody(buf.lineNumbers)
+		final_bytecode += 'END\n'
+	}
+	final_bytecode += '\n'
 	return final_bytecode
 }
 
@@ -457,6 +605,7 @@ export function toAnnotatedBuf(instrBuf: InstructionBuffer, source: string): str
 		bytesInLine += instr.startsWith('jump') ? 1 : 0
 		if (currentLine === -1) {
 			console.log(line, currentByte.toString().padStart(10, '0'), instr.padEnd(50, ' ') + '.')
+			currentByte += bytesInLine
 			continue
 		}
 		if (printSrc) {
